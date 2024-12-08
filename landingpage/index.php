7aa041a4f3d79635c4c0f0a6701476a6abd9ca4c
timotiusabi05 <160981283+timotiusabi05@users.noreply.github.com>
2024-12-08 15:25:39 +0700

    rapiin landing page done

---
+++ landingpage/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eventku</title>
+    <link rel="stylesheet" href="stylesIndex.css">
+</head>
+<body>
+    <div class="header">
+        <h1>Eventku</h1>
+        <div class="auth-buttons">
+            <a href="/eventku/loginregister/landingLogin.php">Masuk</a>
+            <a href="/eventku/loginregister/landingRegister.php">Daftar</a>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="card">
+            <img src="/eventku/landingpage/uploadsLanding/card1.png" alt="Card Atas">
+            <div class="card-content">
+                <h1>Selamat datang di<br>Eventku!</h1>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="/eventku/landingpage/uploadsLanding/card2.png" alt="Card Bawah">
+            <div class="card-content-secondary">
+                <h1>Gabung dan promosikan Eventmu atau UMKM-mu!</h1>
+                <p>Dengan platform Eventku, semuanya lebih mudah!</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
