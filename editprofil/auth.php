d5bab24f2df154a669b82baf1e5db8fc4782f03e
vanvan05 <dyfivan@gmail.com>
2024-12-08 15:55:09 +0700

    edit auth.php

--- editprofil/auth.php
+++ editprofil/auth.php
@@ -13,20 +13,20 @@ if (!isset($_SESSION['id_umkm'])) {
 
 // Periksa apakah profil UMKM sudah ada untuk id_umkm
 $query = "SELECT * FROM profil_umkm WHERE id_umkm = '$id_umkm'";
-$result = mysqli_query($conn, $query);
+$result = mysqli_query($db, $query);
 
 if (mysqli_num_rows($result) == 0) {
     // Inisialisasi Profil UMKM
     $insertQuery = "INSERT INTO profil_umkm (id_umkm) VALUES ('$id_umkm')";
-    if (mysqli_query($conn, $insertQuery)) {
+    if (mysqli_query($db, $insertQuery)) {
       // Inisialisasi Portfolio UMKM
       $portfolioQuery = "INSERT INTO portofolio_umkm (id_profil) 
                          VALUES ('$id_umkm')";
-      if (!mysqli_query($conn, $portfolioQuery)) {
-          echo "Error: " . mysqli_error($conn);
+      if (!mysqli_query($db, $portfolioQuery)) {
+          echo "Error: " . mysqli_error($db);
       }
   } else {
-      echo "Error: " . mysqli_error($conn);
+      echo "Error: " . mysqli_error($db);
   }
 }
 ?>
