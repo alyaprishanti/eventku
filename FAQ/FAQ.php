bd4f8dcca5cd6962948f9e68fa62cce462581d90
Anugerah Hutajulu <160981332+gege010@users.noreply.github.com>
2024-12-08 21:08:12 +0700

    Update FAQ.php

--- FAQ/FAQ.php
+++ FAQ/FAQ.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Satoshi:wght@100..700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="styles/stylesFAQ.css">
+    <link rel="stylesheet" href="/eventku/FAQ/styles/stylesFAQ.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
